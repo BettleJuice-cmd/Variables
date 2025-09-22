bd9f014e891b04cc5a2a4e6451cf8eb9446bcf41
BettleJuice <asdiazm@uamv.edu.ni>
2025-09-22 07:49:10 -0600

    Quedamos a media, continuamos el miercoles.

---
+++ Variables/Modelos/Facultad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables.Modelos
+{
+    public class Facultad
+    {
+        public String Nombre;
+        public String Codigo;
+
+        public String MostrarDatos()
+        {
+            return "Fcultad" + Nombre + ", Codigo" + Codigo;
+        }
+
+
+
+
+    }
+}
